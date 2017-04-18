f4eb555320a81731d85979afe1071ae8201d9b3c
Paula Lee <pdlee877@gmail.com>
2017-04-18 13:23:24 -0700

    initial commit

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var ageField: UITextField!
+    @IBOutlet var placeAge: UILabel!
+    
+    
+//    ! mark tells the program that it will not have a none value or words
+//    but is not efficient and will need edge cases for these later on
+//    but for now we want this
+//    let is better used if we don't change the variable
+    
+//    if thread 1: breakpoint or whatever comes up
+//    makes sure none of the numbers on the side are highlighted blue
+//    because that means you added a breakpoint, so delete it
+    @IBAction func submitAgeButton(_ sender: UIButton) {
+        
+        let ageInCatYears = Int(ageField.text!)! * 7
+        placeAge.text = String(ageInCatYears)
+    }
+    
+    
+    
+    
+//    @IBAction func submitAgeButton(_ sender: UIButton) {
+//        placeAge.text = String(Int(ageField.text!)! * 7)
+//    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
